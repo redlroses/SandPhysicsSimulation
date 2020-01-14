30c2178a9cbfaea7bf856e2fa71b664bd13f6896
redlroses <50994823+redlroses@users.noreply.github.com>
2020-01-14 21:52:52 +0500

    Add localization

---
+++ Assets/Scripts/LangManager.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEngine.UI;
+
+public class LangManager : MonoBehaviour
+{
+    public Image langBttnImg;
+    public Sprite[] flags;
+    public static Lang lng = new Lang();
+
+    private GameObject[] langTexts;
+    private string json;
+    private int langIndex;
+    private string[] langArray = { "ru_RU", "en_US" };
+
+    void Awake()
+    {
+        if (!PlayerPrefs.HasKey("Language"))
+        {
+            if (Application.systemLanguage == SystemLanguage.Russian)
+                PlayerPrefs.SetString("Language", "ru_RU");
+            else PlayerPrefs.SetString("Language", "en_US");
+        }
+        LangLoad();
+        langTexts = GameObject.FindGameObjectsWithTag("localization");
+    }
+    void Start()
+    {
+        for (int i = 0; i < langArray.Length; i++)
+        {
+            if (PlayerPrefs.GetString("Language") == langArray[i])
+            {
+                langIndex = i + 1;
+                langBttnImg.sprite = flags[i];
+                break;
+            }
+        }
+        UpdateLang();
+    }
+
+    void LangLoad()
+    {
+        json = File.ReadAllText(Application.streamingAssetsPath + "/Languages/" + PlayerPrefs.GetString("Language") + ".json");
+        lng = JsonUtility.FromJson<Lang>(json);
+    }
+    public void SwitchBttn()
+    {
+        if (langIndex != langArray.Length) langIndex++;
+        else langIndex = 1;
+        PlayerPrefs.SetString("Language", langArray[langIndex - 1]);
+        langBttnImg.sprite = flags[langIndex - 1];
+        LangLoad();
+        UpdateLang();
+    }
+
+    private void UpdateLang()
+    {
+        foreach (var item in langTexts)
+        {
+            item.GetComponent<LangText>().UpdateLang();
+        }
+    }
+}
+public class Lang
+{
+    public string brush;
+    public string gravity;
+    public string info;
+    public string gradient;
+    public string color1;
+    public string color2;
+    public string smoothGrad;
+    public string type;
+    public string smooth;
+    public string random;
+}
---
+++ Assets/Scripts/LangText.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LangText : MonoBehaviour
+{
+    public string Key;
+
+    private Text text;
+    private Lang lang;
+
+    private void Awake()
+    {
+        text = GetComponent<Text>();
+        //Debug.Log(text.text);
+    }
+
+    private void Start()
+    {
+        lang = LangManager.lng;
+    }
+
+    public void UpdateLang()
+    {
+        lang = LangManager.lng;
+        switch (Key)
+        {
+            case "brush":
+                text.text = lang.brush;
+                break;
+            case "gravity":
+                text.text = lang.gravity;
+                break;
+            case "info":
+                text.text = lang.info;
+                break;
+            case "gradient":
+                text.text = lang.gradient;
+                break;
+            case "color":
+                text.text = lang.color1;
+                break;
+            case "color2":
+                text.text = lang.color2;
+                break;
+            case "smoothGrad":
+                //Debug.Log(text.text);
+                //Debug.Log(lang.smoothGrad);
+                text.text = lang.smoothGrad;
+                break;
+            case "type":
+                text.text = lang.type;
+                break;
+            case "dropdownType":
+                Dropdown dropdown = GetComponent<Dropdown>();
+                Text lable = dropdown.GetComponentInChildren<Text>();
+                if (dropdown.value == 1)
+                {
+                    lable.text = lang.random;
+                }
+                else
+                {
+                    lable.text = lang.smooth;
+                }
+                dropdown.options[0].text = lang.smooth;
+                dropdown.options[1].text = lang.random;
+                break;
+            default:
+                text.text = "ERROR";
+                break;
+        }
+    }
+}
